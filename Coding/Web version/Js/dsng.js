89c59d0cf87190bfb8fff83ae83da90c52b6e52a
Joao Vitor <becao97@gmail.com>
2021-04-22 10:17:13 -0300

    Code folder

---
+++ Coding/Web version/Js/dsng.js
@@ -0,0 +1,18 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+function Principal (){
+    
+}
+
+function Inimigo () {
+    
+}
+
+function animate (){
+    requestAnimationFrame(animate);
+}
