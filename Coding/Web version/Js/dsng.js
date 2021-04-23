2fbe6926434d41d1276ecc8fec25150e52cf9c20
Alunos <equipe.dsng1@gmail.com>
2021-04-23 09:25:58 -0300

    test

--- Coding/Web version/Js/dsng.js
+++ Coding/Web version/Js/dsng.js
@@ -1,7 +1,11 @@
-var canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2D')
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const startBtn = document.querySelector('#startBtn')
+const start = document.querySelector('#start')
 
 var c = canvas.getContext('2d');
 
@@ -16,3 +20,7 @@ function Inimigo () {
 function animate (){
     requestAnimationFrame(animate);
 }
+
+startBtn.addEventListener('click', () => {
+    start.style.display = 'none'
+})
