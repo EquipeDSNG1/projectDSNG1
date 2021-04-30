83c6ed9a57ddb66e85458b08a221fc9460f1a89c
Kennedy Emanoel <kennedy1emanoel19@gmail.com>
2021-04-30 07:58:56 -0300

    Movimentação do personagem

--- Coding/Web version/Js/dsng.js
+++ Coding/Web version/Js/dsng.js
@@ -9,8 +9,14 @@ canvas.height = innerHeight;
 const startBtn = document.querySelector('#startBtn')
 const start = document.querySelector('#start')
 
+var tecla;
+
+window.addEventListener('keydown', function(event) {
+    tecla = event.key;
+})
+
 class Player {
-   
+	
 }
 
 class Projectile {
@@ -43,10 +49,71 @@ class Particle {
 
 }
 
+function Principal (x, y, radius, dx, dy, color){
+
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dx = dx;
+    this.dy = dy;
+    this.color = color;
+
+    this.draw = function(){
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        ctx.stroke();
+        ctx.fill();
+    }
+
+    this.update = function () {
+        if((tecla=='d') || (tecla=='D')) {
+            if(this.x >= 0){
+                this.x = this.x + this.dx;
+            }
+            tecla = '';
+        }
+
+        if((tecla=='a') || (tecla=='A')) {
+            if(this.x <= 800) {
+                this.x = this.x - this.dx;
+            }
+
+            tecla = '';
+        }
+
+        if((tecla=='w') || (tecla=='W')) {
+            if(this.y>=0) {
+                this.y = this.y - this.dy;
+            }
+
+            tecla = '';
+        }
+
+        if((tecla=='s') || (tecla=='S')) {
+            if(this.y <= 800) {
+                this.y = this.y + this.dy;
+            }
+
+            tecla = '';
+        }
+
+        this.draw();
+    }
+}
+
 function animate() {
 	animationId = requestAnimationFrame(animate)
+
+	ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+	bola.update();
 }
+
+var bola = new Principal(10, 10, 10, 10, 10, 'blue');
 
 startBtn.addEventListener('click', () => {
 	start.style.display = 'none'
+	animate();
 })
