f84d04bfe97f1e7c7eba61a9e0481167bdf3d785
Kennedy Emanoel <kennedy1emanoel19@gmail.com>
2021-05-10 08:51:31 -0300

    Coding

--- Coding/Web version/Js/dsng.js
+++ Coding/Web version/Js/dsng.js
@@ -2,12 +2,16 @@ const canvas = document.
 	querySelector('canvas');
 const ctx = canvas.getContext('2d')
 
+//width = 3749;
+//height = 1904;
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 
 var key;
 var key_run;
+
+
 
 window.addEventListener('keydown', function(event) {
     key = event.key;
@@ -19,78 +23,98 @@ window.addEventListener( 'keydown', function( eventrun ) {
   });
 
 class Player {
-	constructor(x,y,radius, v,vr, color){
+	constructor(x,y,v,vr){
 		this.x = x;
         this.y = y;
-        this.radius = radius;
         this.v = v;
 	    this.vr = vr;
-        this.color = color;
 
-    this.draw = function(){
-        ctx.beginPath();
-        ctx.strokeStyle = this.color;
-        ctx.fillStyle = this.color;
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
-        ctx.stroke();
-        ctx.fill();
-    }
+		var spriteWidth = 858;
+		var spriteHeight = 250;
+		var rows = 2;
+		var cols = 6;
+		var width = spriteWidth/cols;
+		var height = spriteHeight/rows;
 
-    this.update = function () {
-        if((key=='d') || (key=='D')) {
-			if(key_run  ==  1){
-				if(this.x-this.radius>=0) {
-					this.x = this.x + this.vr;
+		var curFrame = 0;
+		var frameCount = 6;
+		var srcX = 0;
+		var srcY = 0;
+		var character = new Image();
+		character.src ="img.png";
+
+
+		this.draw = function(){
+
+			//ctx.drawImage(character,srcX,srcY,width,height,this.x,this.y,300,300);
+
+		}
+
+		this.update = function () {
+
+			if((key=='d') || (key=='D')) {
+				if(key_run  ==  1){
+					if(this.x-this.radius>=0) {
+						this.x = this.x + this.vr;
+						
+					}
 				}
-			}
-            if((this.x + this.radius) < canvas.height){
-                this.x = this.x + this.v;
-            }
-
-            key = '';
-        }
-
-        if((key=='A') || (key=='a')) {
-			if(key_run  ==  1){
-				if(this.x-this.radius>=0) {
-					this.x = this.x - this.vr;
+				if((this.x + this.radius) < canvas.height){
+					this.x = this.x + this.v;
+					curFrame = ++curFrame % frameCount;
+					srcX = curFrame * width; 
 				}
+
+				key = '';
 			}
-            if((this.x - this.radius) < canvas.height){
-                this.x = this.x - this.v;
-            }
 
-            key = '';
-        }
+			if((key=='A') || (key=='a')) {
+				if(key_run  ==  1){
+					if(this.x-this.radius>=0) {
+						this.x = this.x - this.vr;
+					}
+				}
+				if((this.x - this.radius) < canvas.height){
+					this.x = this.x - this.v;
+					curFrame = ++curFrame % frameCount;
+					srcX = curFrame * width; 
+				}
 
-        if((key=='w') || (key=='W')) {
-			if(key_run  ==  1){
+				key = '';
+			}
+
+			if((key=='w') || (key=='W')) {
+				if(key_run  ==  1){
+					if(this.y-this.radius>=0) {
+						this.y = this.y - this.vr;
+					}
+				}
 				if(this.y-this.radius>=0) {
-					this.y = this.y - this.vr;
+					this.y = this.y - this.v;
+					curFrame = ++curFrame % frameCount;
+					srcX = curFrame * width; 
 				}
+
+				key = '';
 			}
-            if(this.y-this.radius>=0) {
-                this.y = this.y - this.v;
-            }
 
-            key = '';
-        }
-
-        if((key=='s') || (key=='S')) {
-			if(key_run  ==  1){
-				if(this.y-this.radius>=0) {
-					this.y = this.y + this.vr;
+			if((key=='s') || (key=='S')) {
+				if(key_run  ==  1){
+					if(this.y-this.radius>=0) {
+						this.y = this.y + this.vr;
+					}
 				}
+				if((this.y + this.radius) < canvas.height){
+					this.y = this.y + this.v;
+					curFrame = ++curFrame % frameCount;
+					srcX = curFrame * width; 
+				}
+
+				key = '';
 			}
-            if((this.y + this.radius) < canvas.height){
-                this.y = this.y + this.v;
-            }
 
-            key = '';
-        }
-
-        this.draw();
-    }
+			this.draw();
+		}
 	}
 }
 
@@ -120,8 +144,6 @@ class Particle {
 		this.alpha = 1
 	}
 
-
-
 }
 
 function animate() {
@@ -133,6 +155,6 @@ function animate() {
 }
 
 
-var bola = new Player(100, 100, 10, 6, 9, 'red');
+var bola = new Player(50, 400, 10, 9);
 animate();
 
